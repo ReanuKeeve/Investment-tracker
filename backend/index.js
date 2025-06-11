ec04ae283ce9bffd28edc14b50fb787e70cf6049
ReanuKeeve <taran.in.china@gmail.com>
2025-06-11 20:14:03 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const allowedOrigins = ['https://assets-ctj8.onrender.com'];
 
 const app = express();
-app.options('*', cors({
+app.options('/*', cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
