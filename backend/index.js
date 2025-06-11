cd2ec618644ca5a2b1e99cab1e184537011ff7ad
ReanuKeeve <taran.in.china@gmail.com>
2025-06-11 19:49:45 +0800

    Don't mind me, I'm still fixing backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const allowedOrigins = ['https://assets-ctj8.onrender.com'];
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(cors({
   origin: allowedOrigins,
